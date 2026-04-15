791899cb0b654bc67929906f1263a8448dfbbbac
Rainfall <night.rina@gmail.com>
2026-04-16 04:52:48 +0800

    change: Changing to mng

--- api/ask.js
+++ api/ask.js
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
     const { question } = req.body;
 
     let prompt = `
-      You are an AR AI assistant. Answer the user's question in 1-2 sentences. Be conversational as possible.
+      You are an AR AI assistant. Answer the user's question in 1-2 sentences(in Mongolian). Be conversational as possible.
       You can add movement markers in English:
       - [nod] for nodding head (use at start of sentence)
       - [shake] for shaking head (use at start of sentence)`;
